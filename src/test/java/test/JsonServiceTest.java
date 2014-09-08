52ab9ed5ed527b565b4ab990cc6e711c6839251a
Oleg Shchegula <oleg.shchegula@dataart.com>
2014-09-08 16:06:27 +0300

    fixed cheking json response

--- src/test/java/test/JsonServiceTest.java
+++ src/test/java/test/JsonServiceTest.java
@@ -4,6 +4,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriBuilder;
 
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -25,29 +26,32 @@ public class JsonServiceTest {
 		MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
 		formData.add("email", "111@mailinator.com");
 		formData.add("password", "123456");
-		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON_TYPE)	
+		ClientResponse response = webResource
+				.accept(MediaType.APPLICATION_JSON_TYPE)
 				.header("X-Requested-With", "XMLHttpRequest")
 				.header("Accept-Language", "en-US,en;q=0.8")
-				.post(ClientResponse.class,formData);
-		System.out.println(response.getStatus());
-		System.out.println(response.getEntity(String.class));
-		Assert.assertEquals(response.getStatus(), 200);
+				.post(ClientResponse.class, formData);
 
+		JSONObject obj = new JSONObject(response.getEntity(String.class));
+
+		Assert.assertEquals(obj.get("errors"), false);
+		Assert.assertEquals(response.getStatus(), 200);
 	}
 
 	@Test
 	public void registrationTest() {
 		ClientConfig config = new DefaultClientConfig();
 		Client client = Client.create(config);
-		WebResource webResource = client.resource(UriBuilder.fromUri("http://acc.icpc.org.ua/auth/signup").build());
-		
+		WebResource webResource = client.resource(UriBuilder.fromUri(
+				"http://acc.icpc.org.ua/auth/signup").build());
 
 		MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
 		formData.add("firstNameUk", "test");
 		formData.add("middleNameUk", "test");
 		formData.add("lastNameUk", "test");
-		//formData.add("email", "111@mailinator.com");
-		formData.add("email", "Den" + (int) (Math.random() * 10000 + 1)	+ "@mail.ru");
+		
+		formData.add("email", "Den" + (int) (Math.random() * 10000 + 1)
+				+ "@mail.ru");
 		formData.add("password", "123456");
 		formData.add("passwordRepeat", "123456");
 		formData.add("schoolId", "526e889144579850658b456f");
@@ -55,12 +59,15 @@ public class JsonServiceTest {
 		formData.add("rulesAgree", "1");
 		formData.add("recaptchaIgnore", "1");
 
-		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON_TYPE)	
+		ClientResponse response = webResource
+				.accept(MediaType.APPLICATION_JSON_TYPE)
 				.header("X-Requested-With", "XMLHttpRequest")
 				.header("Accept-Language", "en-US,en;q=0.8")
-				.post(ClientResponse.class,formData);
+				.post(ClientResponse.class, formData);
 		System.out.println(response.getStatus());
-		System.out.println(response.getEntity(String.class));		
+			
+		JSONObject obj = new JSONObject(response.getEntity(String.class));
+		Assert.assertEquals(obj.get("errors"), false);
 		Assert.assertEquals(response.getStatus(), 200);
 	}
 
@@ -68,12 +75,13 @@ public class JsonServiceTest {
 	public void registrationNegativeTest() {
 		ClientConfig config = new DefaultClientConfig();
 		Client client = Client.create(config);
-		WebResource webResource = client.resource(UriBuilder.fromUri("http://acc.icpc.org.ua/auth/signup").build());
+		WebResource webResource = client.resource(UriBuilder.fromUri(
+				"http://acc.icpc.org.ua/auth/signup").build());
 		MultivaluedMap<String, String> formData = new MultivaluedMapImpl();
 		formData.add("firstNameUk", "test");
 		formData.add("middleNameUk", "test");
 		formData.add("lastNameUk", "test");
-		formData.add("email", "111@mailinator.com");		
+		formData.add("email", "111@mailinator.com");
 		formData.add("password", "123456");
 		formData.add("passwordRepeat", "123456");
 		formData.add("schoolId", "526e889144579850658b456f");
@@ -81,12 +89,23 @@ public class JsonServiceTest {
 		formData.add("rulesAgree", "1");
 		formData.add("recaptchaIgnore", "1");
 
-		ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON_TYPE)	
+		ClientResponse response = webResource
+				.accept(MediaType.APPLICATION_JSON_TYPE)
 				.header("X-Requested-With", "XMLHttpRequest")
 				.header("Accept-Language", "en-US,en;q=0.8")
-				.post(ClientResponse.class,formData);
-		System.out.println(response.getStatus());
-		System.out.println(response.getEntity(String.class));		
+				.post(ClientResponse.class, formData);
+		System.out.println(response.getStatus());		
+
+		JSONObject obj = new JSONObject(response.getEntity(String.class));
+
+		Assert.assertTrue((obj.get("errors")).toString().contains("Email is not unique in DB."));
+
 		Assert.assertEquals(response.getStatus(), 200);
+	}
+
+	
+	@Test
+	public void simple(){
+			
 	}
 }
